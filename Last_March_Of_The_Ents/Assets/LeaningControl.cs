8046c9f24df2499aab3f9a6696083e086c3ba594
RhysWarehamPG <rhys.wareham@pushgaming.com>
2023-02-04 23:31:46 +0000

    Body can now tilt and stay intact by using left/right arrows

---
+++ Last_March_Of_The_Ents/Assets/LeaningControl.cs
@@ -0,0 +1,61 @@
+using System.Buffers.Text;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+using UnityEngine.TextCore.Text;
+
+public class LeaningControl : MonoBehaviour
+{
+    public KeyCode rightButton = KeyCode.RightArrow;
+    public KeyCode leftButton = KeyCode.LeftArrow;
+    public float leanSpeed = 10.0f;
+    public float gravity = 9.8f;
+    public float windSpeed = 5.0f;
+    public float windDirection = 1.0f;
+    public float headRadius = 1.0f;
+    public Transform headTransform;
+
+    private Rigidbody2D rb2d;
+    private Vector3 startPosition;
+    private Vector3 currentPosition;
+    private Vector3 headStartPosition;
+    private Vector3 headOvalPosition;
+    private float angle = 0.0f;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+        headStartPosition = headTransform.position;
+    }
+
+    private void Update()
+    {
+        currentPosition = transform.position;
+
+        if (Input.GetKey(rightButton))
+        {
+            rb2d.AddForce(Vector2.right * leanSpeed);
+        }
+        else if (Input.GetKey(leftButton))
+        {
+            rb2d.AddForce(Vector2.left * leanSpeed);
+        }
+
+        rb2d.AddForce(windDirection * windSpeed * Vector2.right * Time.deltaTime);
+        rb2d.AddForce(Vector2.down * gravity * Time.deltaTime);
+
+        angle += Time.deltaTime * windSpeed;
+        headOvalPosition = headStartPosition + new Vector3(headRadius * Mathf.Cos(angle), headRadius * Mathf.Sin(angle), 0.0f);
+        headTransform.position = headOvalPosition;
+
+        if (currentPosition.x > startPosition.x + 0.5f || currentPosition.x < startPosition.x - 0.5f)
+        {
+            windDirection = -windDirection;
+        }
+    }
+}
---
+++ Last_March_Of_The_Ents/Assets/MoveHead.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
+using UnityEditor.Presets;
+using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+using UnityEngine.Animations.Rigging;
+using UnityEngine.InputSystem;
+using Unity.VisualScripting.ReorderableList;
+
+public class MoveHead : MonoBehaviour
+{
+    public Vector2[] points;
+    public float speed;
+    private int lastPointPassed = 0;
+    private int targetPoint = 0;
+    private int move = 0;
+    private bool movingRight = true;
+    private Vector2 targetPosition = new Vector2();
+
+    private void Start()
+    {
+        lastPointPassed = ((points.Length - 1) / 2);
+        targetPoint = lastPointPassed + 1;
+    }
+
+    void Update()
+    {
+        move = 0;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move = -1;
+        }
+
+        if (move != 0)
+        {
+            //If we were moving right, and move is now -1
+            if (movingRight && move == -1)
+            {
+                //Set targetPos to be previous point
+                targetPosition = points[targetPoint - 1];
+                targetPoint = targetPoint - 1;
+
+                movingRight = false;
+            }
+            else if (!movingRight && move == 1)
+            {
+                //Set targetPos to be next point
+                targetPosition = points[targetPoint + 1];
+                targetPoint = targetPoint + 1;
+
+                movingRight = true;
+            }
+
+            targetPosition = points[targetPoint];
+            Vector2 currentPosition = transform.localPosition;
+            Vector2 direction = (targetPosition - currentPosition).normalized;
+
+            transform.localPosition = Vector2.MoveTowards(currentPosition, targetPosition, speed * Time.deltaTime);
+
+            
+            //If distance between current position and target position is less than 0.1f
+            if (Vector2.Distance(currentPosition, targetPosition) < 0.1f)
+            {
+                //If moving right
+                if(movingRight)
+                {
+                    //If not at the end
+                    if (lastPointPassed < points.Length - 1)
+                    {
+                        lastPointPassed = targetPoint;
+                        targetPoint++;
+                    }
+                    else
+                    {
+                        //The ent is falling over!!!!!!!!!!!!!!!
+                    }
+                }
+                else
+                {
+                    //If not at the end
+                    if (lastPointPassed > 0)
+                    {
+                        lastPointPassed = targetPoint;
+                        targetPoint--;
+                    }
+                    else
+                    {
+                        //The ent is falling over!!!!!!!!!!!!!!!
+                    }
+                }
+            }
+            
+        }
+    }
+}
